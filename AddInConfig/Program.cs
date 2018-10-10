921fe90ee48c0ac14ef0ea095aa2e3eb62fefb07
HanRui <54886036@qq.com>
2018-10-10 13:59:21 +0800

    修改所需dll

--- AddInConfig/Program.cs
+++ AddInConfig/Program.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Data.SQLite;
 using System.Linq;
 using System.Text;
- 
+
 
 namespace AddInConfigJson
 {
@@ -12,7 +12,19 @@ namespace AddInConfigJson
         static void Main(string[] args)
         {
             Console.WriteLine("hello");
- 
+
+            SysPluginModel sys = new SysPluginModel();
+            sys.BaseFile = "1";
+            sys.IsOld = true;
+            sys.Name = "234";
+            sys.SavePath = "34";
+            sys.SoftId = 3444444;
+            sys.SoftKey = "c34";
+            sys.SoftSize = "234";
+            sys.Version = "2.34";
+
+            Plugin.AddPlugin(sys);
+
 
             Console.ReadKey();
         }
