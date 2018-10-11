01005191f68889962705fc79ee670a53f3e8f716
HanRui <54886036@qq.com>
2018-10-11 17:43:23 +0800

    修改图片获取方式

--- AddInConfig/Utility/AddImage.cs
+++ AddInConfig/Utility/AddImage.cs
@@ -17,64 +17,86 @@ namespace AddInConfigJson.Utility
         {
             try
             {
-                DownImg(url);
-                string sourceFile = Application.StartupPath + "\\" + filename + "bmp";
-                if (File.Exists(@sourceFile))
+                DownImg(url);//下载图片
+                string sourceFile = Application.StartupPath + "\\" + filename + ".bmp";
+                
+                if (File.Exists(@sourceFile))  //是否有指定配置图片名
                 {
                     string copyFile = Application.StartupPath + "\\" + filename + "bak.bmp";
                     bool isrewrite = true; // true=覆盖已存在的同名文件
                     System.IO.File.Copy(sourceFile, copyFile, isrewrite);
-                }
 
 
-                Bitmap img1 = new Bitmap(Application.StartupPath + "\\zToolbarLargeBak.bmp");
-                Bitmap img2 = new Bitmap(Application.StartupPath + "\\down.png");
- 
-
-                int uniteWidth = img1.Width; //图片统一高度
-                int uniteHeight = img1.Height; //图片统一宽度
-                int uniteWidth2 = img2.Width;
-                Bitmap tableChartImageCol1 = new Bitmap(uniteWidth + uniteWidth2, uniteHeight); //第一行图片
-                List<Image> imageList = new List<Image>();
-                List<Image> imgListCopy = new List<Image>();
-                imageList.Add(img1);
-                imageList.Add(img2);
+                    Bitmap img1 = new Bitmap(copyFile);
+                    Bitmap img2 = new Bitmap(Application.StartupPath + "\\down.png");
 
 
-                foreach (Image i in imageList) //重新new图片,改变图片高度宽度
-                {
-                    Bitmap b = new Bitmap(i, uniteWidth, uniteHeight);
-                    imgListCopy.Add(b);
-                }
+                    int uniteWidth = img1.Width; //图片统一高度
+                    int uniteHeight = img1.Height; //图片统一宽度
+                    int uniteWidth2 = img2.Width;
+                    Bitmap tableChartImageCol1 = new Bitmap(uniteWidth + uniteWidth2, uniteHeight); //第一行图片
+                    List<Image> imageList = new List<Image>();
+                    List<Image> imgListCopy = new List<Image>();
+                    imageList.Add(img1);
+                    imageList.Add(img2);
 
-                for (int i = 0; i < imageList.Count; i++)
-                {
-                    if (imageList.Count - 1 >= i)
+
+                    foreach (Image i in imageList) //重新new图片,改变图片高度宽度
                     {
-                        imageList[i] = imgListCopy[i]; //更改后的图片，赋给原图片
+                        Bitmap b = new Bitmap(i, uniteWidth, uniteHeight);
+                        imgListCopy.Add(b);
                     }
+
+                    for (int i = 0; i < imageList.Count; i++)
+                    {
+                        if (imageList.Count - 1 >= i)
+                        {
+                            imageList[i] = imgListCopy[i]; //更改后的图片，赋给原图片
+                        }
+                    }
+
+                    if (true) //进去拼图 //横向拼接
+
+                    {
+                        //图片白板1
+                        Graphics graph = Graphics.FromImage(tableChartImageCol1);
+
+                        //初始化这个大图
+                        graph.DrawImage(tableChartImageCol1, uniteWidth + uniteWidth2, uniteHeight);
+
+                        //初始化当前宽
+                        int currentWidth = 0;
+                        graph.Clear(System.Drawing.Color.White); ////清除画布,背景设置为白色
+                        foreach (Image i in imageList)
+                        {
+                            graph.DrawImage(i, currentWidth, 0); //拼图--图片拼起来
+                            currentWidth += i.Width; //拼接改图后,当前宽度
+                        }
+                    }
+                    tableChartImageCol1.Save(AppDomain.CurrentDomain.BaseDirectory + "\\" + filename + "bmp");
                 }
-
-                if (true) //进去拼图 //横向拼接
-
+                else
                 {
+                    Bitmap img1 = new Bitmap(Application.StartupPath + "\\down.png");
+                    int uniteWidth = img1.Width; //图片统一高度
+                    int uniteHeight = img1.Height; //图片统一宽度
+                    Bitmap tableChartImageCol1 = new Bitmap(uniteWidth, uniteHeight); //第一行图片
                     //图片白板1
                     Graphics graph = Graphics.FromImage(tableChartImageCol1);
 
                     //初始化这个大图
-                    graph.DrawImage(tableChartImageCol1, uniteWidth + uniteWidth2, uniteHeight);
+                    graph.DrawImage(tableChartImageCol1, uniteWidth , uniteHeight);
 
                     //初始化当前宽
                     int currentWidth = 0;
                     graph.Clear(System.Drawing.Color.White); ////清除画布,背景设置为白色
-                    foreach (Image i in imageList)
-                    {
-                        graph.DrawImage(i, currentWidth, 0); //拼图--图片拼起来
-                        currentWidth += i.Width; //拼接改图后,当前宽度
-                    }
+                    graph.DrawImage(img1, currentWidth,0); //拼图--图片拼起来
+                    currentWidth = uniteWidth; //拼接改图后,当前宽度
+                    tableChartImageCol1.Save(AppDomain.CurrentDomain.BaseDirectory + "\\" + filename + ".bmp");
+
                 }
 
-                tableChartImageCol1.Save(AppDomain.CurrentDomain.BaseDirectory + "\\zToolbarLarge.bmp");
+                
             }
             catch (Exception e)
             {
