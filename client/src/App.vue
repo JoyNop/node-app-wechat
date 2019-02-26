ca30790b832c1e8315b967fe3a089eea28ecec81
HanRui <597589373@qq.com>
2019-02-26 11:10:31 +0800

    配置vuex

--- client/src/App.vue
+++ client/src/App.vue
@@ -3,6 +3,19 @@
     <router-view/>
   </div>
 </template>
+<script>
+import jwt_decode from "jwt-decode";
+export default {
+  name:'app',
+  created(){
+    if(localStorage.wxpyqToken){
+      const decode=jwt_decode(localStorage.wxpyqToken);
+      this.$store.dispatch("setUser",decode)
+    }
+  }
+}
+</script>
+
 <style>
 html,
 body,
--- client/src/main.js
+++ client/src/main.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import axios from "./http";
+import store from "./store";
 
 
 Vue.config.productionTip = false
@@ -9,5 +10,6 @@ Vue.prototype.$axios = axios
 
 new Vue({
   router,
+  store,
   render: h => h(App)
 }).$mount('#app')
