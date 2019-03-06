0ece69b77adea3fb9a8686b6296f28806edaea45
HanRui <597589373@qq.com>
2019-03-06 15:15:47 +0800

    11

--- config/keys.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  // mongoURI: 'mongodb://test:123abc@ds227853.mlab.com:27853/wchart-api',
-  mongoURI: 'mongodb://test:test0000@ds011765.mlab.com:11765/node-vue-wechat',
-  secretOrKey: 'secret'
-};
